0442fb3007f7ed10301066d5efc38fa5364d7791
黄渗茹 <1098751484@qq.com>
2026-03-05 17:50:18 +0800

    astro set name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://huangshenru.github.io',      // 你的 GitHub Pages 根地址
+  base: '/my-astro',                           // 仓库名，必须带斜杠
+  // ... 其他配置
+})
